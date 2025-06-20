ed6b5e1db6205dcd3f551e35351ca21f1e445fcc
Michael Fiedler <michael.fiedler1604@gmail.com>
2025-06-20 07:17:27 +0200

    Initial Commit

---
+++ Notenrechner/NotenrechnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotenrechnerApp.swift
+//  Notenrechner
+//
+//  Created by Michael Fiedler on 20.06.25.
+//
+
+import SwiftUI
+
+@main
+struct NotenrechnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
